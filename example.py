0ab3a186f935f6ac297154181fe4c9761bcf0cb3
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-09-28 14:05:14 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,5 @@
+#!/usr/bin/env python3
 import shortener
-
-result = []
-result.append(shortener.shortener("youtube.com", "https://seggly.uk/short"))
-result.append(shortener.shortener("google.com", "https://seggly.uk/short"))
-
-for res in result:
-    print("Shortened url at: " + res)
 
 result = []
 
