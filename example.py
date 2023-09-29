aedcbf01eabbf97597ca5029d70dff6a6247fbef
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-09-29 09:31:25 +0200

    ren

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import shortener
 
 result = []
 
-result.append(shortener.shortener("domain.com", "doma.in"))
+result.append(shortener.shorten("domain.com", "doma.in"))
 
 for res in result:
     print("Shortened url at: " + res)
