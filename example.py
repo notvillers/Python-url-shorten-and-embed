1adcb2611b290285db48e2266d99734e4a8d116e
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-10-03 17:39:43 +0200

    switch to uuid4

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import shortener
 
 result = []
 
-result.append(shortener.shorten("domain.com", "doma.in"))
+result.append(shortener.shorten("https://github.com/notvillers/Python-url-shortener", "doma.in", "", True))
 
 for res in result:
     print("Shortened url at: " + res)
--- shortener.py
+++ shortener.py
@@ -1,59 +1,40 @@
 #!/usr/bin/env python3
 import os
-import sqlite3
-import create_db
-
-db_file = create_db.db_name() # creates .db if not found
-if not os.path.exists(db_file):
-    create_db.create()
-connection = sqlite3.connect(db_file)
-cursor = connection.cursor()
-
-path = os.path.dirname(__file__) # Path to the redirect files, for example with Apache2: /var/www/html/
-
-def select(text): # selects from the .db
-    cursor.execute(text)
-    return cursor.fetchall()
-
-def insert(text): # inserts into the .db
-    cursor.execute(text)
-    connection.commit()
+import uuid
 
 def create_html(text): # creates the html
     skeleton = [
-        '<!DOCTYPE html> <html> <head> <title>SEGGLY redirect</title> <meta http-equiv = "refresh" content = "0; url = ',
+        '<!DOCTYPE html> <html> <head> <title>SEGGLY redirect</title> <meta http-equiv = "refresh" content = "0; url =  ',
         '" /> </head> <body> <p>Redirecting...</p> </body> </html>'
     ]
     return skeleton[0] + text + skeleton[1]
 
-def shorten(link, url_pref): # main
+def create_mp4_emb(text): # creates the html for the webplayer
+    skeleton = [
+        '<!DOCTYPE html><html><body style="text-align: center"><h1>P(segg)layer</h1><video controls="controls"><source src="',
+        '" type="video/mp4" /></video></body></html>'
+    ]
+    return skeleton[0] + text + skeleton[1]
 
-    shorts = []
+def shorten(link, url_pref, path, mp4): # main
 
-    for row in select("select * from direction"):
-        shorts.append(row)
+    if path == "":
+        path = os.path.dirname(__file__) # Path to the redirect files, for example with Apache2: /var/www/html/
 
-    max = 0
-    for short in shorts:
-        if int(short[1]) > int(max):
-            max = int(short[1])
+    if mp4 == False:
+        
+        next = str(uuid.uuid4())
 
-    next = max + 1
-    shorts.append([link, str(next)])
-    insert("INSERT INTO direction (src, dest) values ('" + link + "', " + str(next) + ")")
-
-    short_nums = []
-
-    for file in [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]:
-        if file.endswith('.html'):
-            short_nums.append(file.replace('.html',''))
-
-    url = ""
-
-    for short in shorts:
-        if short[1] not in short_nums:
-            with open(path + "/" + short[1], 'w') as file:
-                file.write(create_html(short[0]))
-                url = url_pref + "/" + short[1]
+        with open(path + "/" + next, 'w') as file:
+            file.write(create_html(link))
+            url = url_pref + "/" + next
     
+    elif mp4 == True:
+
+        next = str(uuid.uuid4())
+
+        with open(path + "/" + next, 'w') as file:
+            file.write(create_mp4_emb(link))
+            url = url_pref + "/" + next
+
     return url
